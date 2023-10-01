93200476bfe0fcaa6fe25b2946c9b63ef13d94df
Patrik <144246738+PatpetEdu@users.noreply.github.com>
2023-10-01 22:24:29 +0200

    ändrat kod och kommentarer

--- Program.cs
+++ Program.cs
@@ -1,39 +1,38 @@
-﻿namespace NumbersGame2
+﻿namespace NumbersGame2                              //**Patrik Petterson NET23**//
 {
-    internal class Program
+    internal class Program    
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök");
+          //Information to user
+            Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök.");
             Console.WriteLine();
             Console.WriteLine("skriv in ditt nummer:");
 
 
             Program program = new Program();         
-            int randNumber = program.NumberGenerator();
+            int randNumber = program.NumberGenerator(); //Calls method NumberGenerator
 
-            int guess = program.CheckGuess(randNumber);
-            Console.WriteLine(guess);
-
+            int guess = program.CheckGuess(randNumber); //Calls method CheckGuess       
         }
-        public int NumberGenerator()
+        public int NumberGenerator() //Method NumberGenerator
         {
-            Random random = new Random();
-            int randNumber = random.Next(1, 11);
+            Random random = new Random(); 
+            int randNumber = random.Next(1, 20); //Stores a randomized number in int randNumber
             return randNumber;
         }
-        public int CheckGuess(int secretNumber)
+        public int CheckGuess(int secretNumber) //Metho CheckGuess
         {
-            int maxTries = 5;
+            int maxTries = 5; //Numbers of tries the user have to guess right number
             int Tries = 0;
 
-            while (Tries < maxTries)
+            while (Tries < maxTries) //Loop to control number of tries
             {
-                string userInput = Console.ReadLine();
+                string userInput = Console.ReadLine(); //Reads user input
 
-                if (int.TryParse(userInput, out int guess))
+                if (int.TryParse(userInput, out int guess))  //converts string to int
                 {
-                    if (guess == secretNumber)
+                    if (guess == secretNumber) //checks the user guess against randomnumber 
                     {
                         Console.WriteLine($"Wohoo! Du klarade det! Med {maxTries - Tries} försök tillgodo!");
                         return guess;
